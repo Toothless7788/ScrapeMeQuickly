b423694e778f31732683770c5aaeab74a7d81632
w45242hy <hin.yip-2@manchester.ac.uk>
2025-04-05 12:22:32 +0100

    feat: create Maven project

---
+++ ScrapeMeQuickly_Java/src/main/java/com/hackathon/java/Sandbox.java
@@ -0,0 +1,11 @@
+package com.hackathon.java;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Sandbox {
+	public static void main(String[] args) {
+		String html = "<html><head><title>First parse</title></head><body><p>Parsed HTML into a doc.</p></body></html>";
+		Document document = Jsoup.parse(html);
+	}
+}
