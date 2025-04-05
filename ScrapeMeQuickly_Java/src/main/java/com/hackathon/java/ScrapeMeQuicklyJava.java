e35396c1927bd37ebea068dc37a04e06cc2ead52
w45242hy <hin.yip-2@manchester.ac.uk>
2025-04-05 14:47:09 +0100

    feat: upate Scraper

--- ScrapeMeQuickly_Java/src/main/java/com/hackathon/java/ScrapeMeQuicklyJava.java
+++ ScrapeMeQuickly_Java/src/main/java/com/hackathon/java/ScrapeMeQuicklyJava.java
@@ -1,5 +1,7 @@
 package com.hackathon.java;
 
 public class ScrapeMeQuicklyJava {
-
+	public static void main(String[] args) {
+		
+	}
 }
--- ScrapeMeQuickly_Java/src/main/java/com/hackathon/java/entity/Scraper.java
+++ ScrapeMeQuickly_Java/src/main/java/com/hackathon/java/entity/Scraper.java
@@ -7,17 +7,26 @@ import java.util.Map;
 
 public class Scraper implements Runnable {
 	private List<Integer> years;
-	private double priceSum;
-	private int carNum;
+	private double priceSum = 0;
+	private int carNum = 0;
 	private Map<String, Integer> makesCounts;    // In the form of {make_name, make_count} - for mode make
 	private int minCarInedx;
 	private int maxCarIndex;
 	private String url;    // The url of the target server to get data from
-	private String proxyUrl;    // The url of the proxy server
+	private String proxyHost;    // The host of the proxy server
+	private int proxyPort;    // The port of the host of the proxy server
+	private String proxyUsername;
+	private String proxyPassword;
 	
-	public Scraper(int minCarIndex, int maxCarIndex, String url, String proxyUrl) {
+	public Scraper(int minCarIndex, int maxCarIndex, String url, String proxyHost, int proxyPort, String proxyUsername, String proxyPassword) {
 		this.minCarInedx = minCarIndex;
 		this.maxCarIndex = maxCarIndex;
+		this.url = url;
+		// Proxy
+		this.proxyHost = proxyHost;
+		this.proxyPort = proxyPort;
+		this.proxyUsername = proxyUsername;
+		this.proxyPassword = proxyPassword;
 		
 		years = new ArrayList<Integer>();
 		makesCounts = new HashMap<String, Integer>();
